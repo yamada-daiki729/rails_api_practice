9496a35acf2b15c5b11239af7fbf54fd6200f439
daiki_yamada <aristrat@icloud.com>
2023-05-28 17:28:08 +0900

    article コントローラーのshowレスポンス実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -5,8 +5,14 @@ module Api
     class ArticlesController < BaseController
       def index
         articles = Article.all
-        json_string = ArticleSerializer.new(articles).serializable_hash.to_json
-        render json: json_string
+        json_articles = ArticleSerializer.new(articles).serializable_hash.to_json
+        render json: json_articles
+      end
+
+      def show
+        article = Article.find(params[:id])
+        json_article = ArticleSerializer.new(article)
+        render json: json_article
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, format: 'json' do
     namespace :v1 do
-      resources :articles, only: :index
+      resources :articles, only: [:index, :show]
     end
   end
 end
