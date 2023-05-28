21f6af4dee265a0bbdd2695237ceb9a2ef7dcbaa
daiki_yamada <aristrat@icloud.com>
2023-05-28 16:08:03 +0900

    lint微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-
   has_one :social_profile, dependent: :destroy
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :name,  presence: true
+  validates :name, presence: true
   validates :email, presence: true
 end
