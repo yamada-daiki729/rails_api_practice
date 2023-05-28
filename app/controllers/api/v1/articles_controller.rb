d45d1e61ce0c9d0080e9ee70ba40aa3092ef1939
daiki_yamada <aristrat@icloud.com>
2023-05-28 17:13:08 +0900

    lintチェック微修正

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -5,7 +5,6 @@ module Api
     class ArticlesController < BaseController
       def index
         articles = Article.all
-        byebug
         json_string = ArticleSerializer.new(articles).serializable_hash.to_json
         render json: json_string
       end
