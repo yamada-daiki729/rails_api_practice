ad93cc91df6d89c36bbdf30460d57f975cda58d1
daiki_yamada <aristrat@icloud.com>
2023-06-02 19:15:29 +0900

    lint微修正2

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -10,16 +10,18 @@ module Api
 
       protected
 
-      # rubocop:disable Naming/MemoizedInstanceVariableName
       def authenticate
         authenticate_or_request_with_http_token do |token, _options|
-          @_current_user ||= Apikey.still_valid.find_by(access_token: token)&.user
+          set_current_user(token)
         end
       end
-      # rubocop:enable Naming/MemoizedInstanceVariableName
 
       def current_user
         @_current_user
+      end
+
+      def set_current_user(token)
+        @_current_user ||= Apikey.still_valid.find_by(access_token: token)&.user
       end
 
       def set_access_token!(user)
